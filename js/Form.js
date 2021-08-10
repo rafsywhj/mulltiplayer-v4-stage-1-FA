d7102edd759fceafc7aedd95111bdae74424d7ea
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-08-10 05:51:10 +0530

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -8,7 +8,7 @@ class Form {
 
   setElementsPosition() {
     this.titleImg.position(120, 100);
-    this.input.position(width / 2 - 110, height / 2 - 80);
+    this.input.position(width / 2 - 110, height / 2 - 40);
     this.playButton.position(width / 2 - 90, height / 2 - 20);
     this.greeting.position(width / 2 - 300, height / 2 - 100);
   }
